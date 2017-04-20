af60dd27f458599fab6591ca0feee6315cd1cdef
elongxu <xuyilong@chinasofti.com>
2017-04-20 11:15:41 +0800

    android 数据库雏形

---
+++ app/src/main/java/com/example/lpf/databasemodule/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lpf.databasemodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 本工程用于总结数据库模块
+ * 目的：下一个工程能够快速的使用数据库，建立高效的数据库。
+ * 描述：前期主要总结使用contentProvider对数据库的操作
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
